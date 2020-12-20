ad6d296e56d33991f590ec8875179f18b9ee83ad
soninv <sonin.vitalii@gmail.com>
2020-12-20 02:55:44 +0100

    localStorage_final
    j'ai trouvé le petit problem dans le vesrion precedent

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -3,11 +3,11 @@ import {TodoListData} from './dataTypes/TodoListData';
 import {Observable, BehaviorSubject} from 'rxjs';
 import {TodoItemData} from './dataTypes/TodoItemData';
 
+var stock;
 @Injectable()
 export class TodoService {
 
   private todoListSubject = new BehaviorSubject<TodoListData>( {label: 'TodoList', items: []} );
-  
   constructor() { }
 
   getTodoListDataObserver(): Observable<TodoListData> {
@@ -22,6 +22,16 @@ export class TodoService {
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label, isDone: I.isDone, editing: false}) )
     });
   }
+  // on enregistre le tableau d'item dans le local storage
+  getLocalStorageItemsTodolist(): TodoItemData[] {
+    const localStorageItem = JSON.parse(localStorage.getItem('items'));
+    return localStorageItem == null ? [] : localStorageItem.items;
+  }
+
+  // on recupere le tableau d'item dans le local storage
+  setLocalStorageItemsTodolist(items: TodoItemData[] ): void {
+    localStorage.setItem('items', JSON.stringify({ items: items }));
+  }
 
   //Valide ou invalide les items passes en argument
   setItemsDone(isDone: boolean, ...items: TodoItemData[] ) {
@@ -30,7 +40,8 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone, editing: false}) )
     });
-    localStorage.setItem("items", JSON.stringify({ items: items } ));
+    this.setLocalStorageItemsTodolist(this.todoListSubject.value.items);
+    //stock = localStorage.setItem("items", JSON.stringify({ items: items } ));
   }
 
   //Passe en mode editing ou non editing les items passes en argument
@@ -40,8 +51,12 @@ export class TodoService {
       label: tdl.label,
       items: tdl.items.map( I => items.indexOf(I) === -1 ? I : ({label: I.label, isDone: I.isDone, editing: isEditing}) )
     });
-    localStorage.setItem("items", JSON.stringify({ items: items } ));
+    this.setLocalStorageItemsTodolist(this.todoListSubject.value.items);
+   // stock = localStorage.setItem("items", JSON.stringify({ items: items } ));
   }
+
+  
+
 
   //Ajoute les items passes en argument a la liste des items
   appendItems( ...items: TodoItemData[] ) {
@@ -50,7 +65,8 @@ export class TodoService {
       label: tdl.label, // ou on peut écrire: ...tdl,
       items: [...tdl.items, ...items]
     });
-    localStorage.setItem("items", JSON.stringify({ items: items } ));
+    this.setLocalStorageItemsTodolist(this.todoListSubject.value.items);
+   // stock = localStorage.setItem("items", JSON.stringify({ items: items } ));
   }
 
     //Retire les items passes en argument de la liste
